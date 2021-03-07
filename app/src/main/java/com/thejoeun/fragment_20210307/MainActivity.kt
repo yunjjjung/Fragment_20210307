b6b2fc9f9a81bf845d02b88b2050ac905661f4c8
최윤정 <jjjung1003@naver.com>
2021-03-07 11:08:55 +0900

    Initial commit

---
+++ app/src/main/java/com/thejoeun/fragment_20210307/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.thejoeun.fragment_20210307
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    
+    override fun setupEvents() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setValues() {
+        TODO("Not yet implemented")
+    }
+
+
+
+}
