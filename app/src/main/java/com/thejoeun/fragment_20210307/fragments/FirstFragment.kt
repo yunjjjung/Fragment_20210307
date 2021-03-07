db3af0bf40c7f6ba66443074249f7d3b5f54bc79
최윤정 <jjjung1003@naver.com>
2021-03-07 11:30:23 +0900

    fragment 내부의 이벤트 처리

--- app/src/main/java/com/thejoeun/fragment_20210307/fragments/FirstFragment.kt
+++ app/src/main/java/com/thejoeun/fragment_20210307/fragments/FirstFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.thejoeun.fragment_20210307.R
+import kotlinx.android.synthetic.main.fragment_first.*
 
 class FirstFragment : Fragment() {
 
@@ -17,6 +19,16 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        toastBtn.setOnClickListener {
+
+            Toast.makeText(activity, "프레그먼트를 통한 토스트", Toast.LENGTH_SHORT).show()
+
+        }
     }
 
 }
